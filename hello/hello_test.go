d030e6f0c5ef06441be6d07154f006af597ec414
Vitaly Pobokin <pbk.vitaliy@gmail.com>
2020-02-24 15:53:56 +0300

    Completed module hello

---
+++ hello/hello_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("pbk", "")
+		want := "Hello, pbk"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Amelie", "French")
+		want := "Bonjour, Amelie"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
